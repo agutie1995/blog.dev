8ab54abcb4d4b9cfe62451f734d07873dbfba662
Alexandra Gutierrez <agutie95@yahoo.com>
2015-08-19 10:38:20 -0500

    added routes for /route and /portfolio

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,22 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('/sayhello/{name?}', function($name = null)
+{
+    if ($name == "Chris") {
+        return Redirect::to('/');
+    } else {
+        return "Hello, $name!";
+    }
+});
+
+Route::get('/resume', function()
+{
+    return "This is my resume.";
+});
+
+Route::get('/portfolio', function()
+{
+    return "This is my portfolio.";
+});
