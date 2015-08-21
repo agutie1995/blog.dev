5372c67de1ff3e02da97b9b519d59f12e791c4aa
Alexandra Gutierrez <agutie95@yahoo.com>
2015-08-21 15:54:56 -0500

    created posts seeder and faker to populate databases

--- app/database/seeds/PostsSeeder.php
+++ app/database/seeds/PostsSeeder.php
@@ -1,22 +1,35 @@
 <?php
 
+use Faker\Factory as Faker;
+
 class PostsSeeder extends Seeder
 {
 	public function run()
 	{
-		$post = new Post();
-		$post->title = 'My First Post';
-		$post->body = 'This is my first post for my Laravel blog';
-		$post->save();
+		$faker = Faker::create();
 
-		$post1 = new Post();
-		$post1->title = 'Dear Diary...';
-		$post1->body = 'Don\'t you hate it when you put files in the wrong folder?';
-		$post1->save();
+		Post::truncate();
 
-		$post2 = new Post();
-		$post2->title  = 'Blah, blahblah, blahblah';
-		$post2->body = 'Blaaaaahhh';
-		$post2->save();
+		for ($i = 0; $i<20; $i++){
+			$post = new Post();
+			$post->title = $faker->catchPhrase;
+			$post->body = $faker->bs;
+			$post->save();
+		}
+
+		// $post = new Post();
+		// $post->title = 'My First Post';
+		// $post->body = 'This is my first post for my Laravel blog';
+		// $post->save();
+
+		// $post1 = new Post();
+		// $post1->title = 'Dear Diary...';
+		// $post1->body = 'Don\'t you hate it when you put files in the wrong folder?';
+		// $post1->save();
+
+		// $post2 = new Post();
+		// $post2->title  = 'Blah, blahblah, blahblah';
+		// $post2->body = 'Blaaaaahhh';
+		// $post2->save();
 	}
 }
