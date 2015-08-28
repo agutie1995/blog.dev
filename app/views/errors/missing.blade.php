3ff51ef0c58eda3d0bf791e7ed5f1e9587d0e7ad
Alexandra Gutierrez <agutie95@yahoo.com>
2015-08-28 09:13:23 -0500

    edited 4040 picture

--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -1,4 +1,5 @@
 @extends ('layouts.master')
-@section('style')
+@section('content')
 	<link rel="stylesheet" type="text/css" href="{{ URL::asset('/css/missing.css') }}">
+	<img id="four-oh-four" src="/img/tfios_okay.jpg">
 @stop
