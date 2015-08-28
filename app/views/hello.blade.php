bd54826f10684d6491bec59572ee1424207c1c49
Alexandra Gutierrez <agutie95@yahoo.com>
2015-08-28 16:06:38 -0500

    made cover responsive

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -1,24 +1,31 @@
 <head>
     <!-- Latest compiled and minified CSS -->
-    {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> --}}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
 
-    <link rel="stylesheet" href="/css/hello.css">;
+    <link rel="stylesheet" href="/css/hello.css">
 </head>
 <body>
-    <div id="backstretch">
-        <img id="cover-photo" class="img-responsive" alt="Null" src="/img/cover_photo.jpg">
-    </div>
-        <div id="cover-text">
-            <h1 class="cover-font img-responsive">Alexandra Gutierrez</h1>
-            <h3 class="cover-font lead img-responsive"> Full-Stack Web Developer</h3>
-            <a href="{{{ action('HomeController@showResume') }}}" id="learn-more" class="cover-font img-responsive" >Learn more</a>
-        </div> 
+    <div class="container">
+        <div class="col-md-7">
+            <div id="cover-text">
+                <h1 class="cover-font img-responsive">Alexandra Gutierrez</h1>
+                <h3 class="cover-font img-responsive">Full-Stack Web Developer</h3><br>
+                <a id="learn-more" class="cover-font img-responsive" href="{{{ action('HomeController@showResume') }}}">Learn more</a>
+            </div> 
+        </div>
+
+        <div class="col-md-5">
+            <div id="img-backstretch">
+                <img id="cover-photo" alt="Null" src="/img/seniorPic.jpg">
+            </div>
+        </div>
         <a id="photo-by" class="img-responsive" href="http://thestreetphotog.blogspot.com/" target="_blank">Photo By: Jesse Acosta</a>
 
+    </div>
     <script src="/js/jquery.js"></script>
     <script type="text/javascript" src="https://code.jquery.com//jquery-2.1.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
