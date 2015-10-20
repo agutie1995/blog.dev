b285ff381dd0ed8e59aedb5a9a9a1675666fcbf8
Alexandra Gutierrez <agutie95@yahoo.com>
2015-10-20 17:43:44 -0500

    edited css and changed picture on portfolio

--- app/views/calculator.php
+++ app/views/calculator.php
@@ -3,72 +3,79 @@
 <html>
 
 <head>
-
     <title>Calculator</title>
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/calculator.css">
-
 </head>
 
 <body>
-
     <section>
-
-        <div id="container">
-
+        <div id="container" class="col-md-12">
             <div id="display">
+                <div class="row">
+                    <input id="leftTextBox" class="textBoxes" name="left" type="text" readonly>
 
-                <input id="leftTextBox" class="textBoxes" name="left" type="text" readonly>
-
-                <input id="middleTextBox" class="textBoxes" name="middle" type="text" readonly>
-                
-                <input id="rightTextBox" class="textBoxes" name="right" type="text" readonly>
+                    <input id="middleTextBox" class="textBoxes" name="middle" type="text" readonly>
+                    
+                    <input id="rightTextBox" class="textBoxes" name="right" type="text" readonly>
+                </div>
 
             </div>
 
             <div id="buttons">
+                <div class="row">
+                    <button id="clear" class="button">C</button>
+                    <div class="solarPanel">
+                        <img class="solarPanel" src="img/solar_panels.jpg">
+                    </div>
+                </div>
 
-                <button id="numOne" class="button">1</button>
+                <div class="row">
+                    <button id="numOne" class="button">1</button>
 
-                <button id="numTwo" class="button">2</button>
+                    <button id="numTwo" class="button">2</button>
 
-                <button id="numThree" class="button">3</button>
+                    <button id="numThree" class="button">3</button>
 
-                <button id="add" class="button">+</button>
+                    <button id="add" class="button">+</button>
+                </div>
 
-                <button id="numFour" class="button">4</button>
+                <div class="row">
+                    <button id="numFour" class="button">4</button>
 
-                <button id="numFive" class="button">5</button>
+                    <button id="numFive" class="button">5</button>
 
-                <button id="numSix" class="button">6</button>
+                    <button id="numSix" class="button">6</button>
 
-                <button id="subtract" class="button">-</button>
+                    <button id="subtract" class="button">-</button>
+                </div>
 
-                <button id="numSeven" class="button">7</button>
+                <div class="row">
+                    <button id="numSeven" class="button">7</button>
 
-                <button id="numEight" class="button">8</button>
+                    <button id="numEight" class="button">8</button>
 
-                <button id="numNine" class="button">9</button>
+                    <button id="numNine" class="button">9</button>
 
-                <button id="multiply" class="button">*</button>
+                    <button id="multiply" class="button">*</button>
+                </div>
 
-                <button id="clear" class="button">C</button>
-                
-                <button id="numZero" class="button">0</button>
+                <div class="row">
+                    <button id="decimal" class="button">.</button>
+                    
+                    <button id="numZero" class="button">0</button>
 
-                <button id="equal" class="button">=</button>
+                    <button id="equal" class="button">=</button>
 
-                <button id="divide" class="button">/</button>
+                    <button id="divide" class="button">/</button>
+                </div>
 
             </div>
         </div>
-
     </section>
 
-<script type="text/javascript" src="https://code.jquery.com//jquery-2.1.1.min.js"></script>
-<script src="/js/calculator.js">
-</script>
+    <script src="/js/calculator.js"></script>
 
 </body>
-
 </html>
